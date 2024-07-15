74eab428957bc59197d1859debfe8b04cd9ad87a
unknown <cabilesjarold@gmail.com>
2024-07-15 16:25:40 +0700

    first commit

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>sd</h1>
+    <button>Home</button>
+    <button>Rooms</button>
+    <button>About Us</button>
+    <button>Contacts</button>
+    <button>Booking</button>
+  </div>
+</template>
+
+<style scope>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TopHeader from '@/components/TopHeader.vue'
+</script>
+
+<template>
+  <div>
+    <TopHeader />
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<style scoped>
+body {
+  background-color: blue;
+}
+</style>
